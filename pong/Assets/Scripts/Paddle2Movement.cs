fbe568962ab4b1ec8d91422b20d981b0ce0b4f6b
gregelious <iftakar201@gmail.com>
2023-09-19 11:47:53 -0400

    added second paddle script, works

--- pong/Assets/Scripts/Paddle2Movement.cs
+++ pong/Assets/Scripts/Paddle2Movement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Paddle2Movement : MonoBehaviour
 {
+
+    public float speed = 1f;
+    public float yBorder = 4.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = .05f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W) && transform.position.y <= yBorder)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + speed);
+        }
+        if (Input.GetKey(KeyCode.S) && transform.position.y >= -yBorder)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - speed);
+        }
     }
 }
--- pong/Assets/Scripts/PaddleMovement.cs
+++ pong/Assets/Scripts/PaddleMovement.cs
@@ -10,7 +10,7 @@ public class PaddleMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = .01f;
+        speed = .05f;
     }
 
     // Update is called once per frame
